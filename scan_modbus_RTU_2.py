9d93b01e06e2252ab109fc64ae6351835bdd2012
novakma7 <martin.novak@fs.cvut.cz>
2024-08-02 10:48:22 +0200

    Add files via upload

---
+++ scan_modbus_RTU_2.py
@@ -0,0 +1,46 @@
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+import logging
+
+# Configure logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.WARNING)  # Disable debug logging
+
+# Configure the client
+client = ModbusClient(method='rtu', port='COM6', baudrate=9600, timeout=1, parity='N', stopbits=1, bytesize=8)
+
+# Connect to the client
+connection = client.connect()
+if not connection:
+    print("Failed to connect to the Modbus device")
+else:
+    # Function to read a single input register
+    def read_single_input_register(client, unit_id, address):
+        try:
+            result = client.read_input_registers(address=address, count=1, unit=unit_id)
+            if result.isError():
+                print(f"Register {address} not exists")
+            else:
+                return result.registers[0]
+        except Exception as e:
+            print(f"Register {address} not exists")
+
+    # Function to scan registers within a range
+    def scan_registers(client, unit_id, start_address, end_address):
+        for address in range(start_address, end_address + 1):
+            register_value = read_single_input_register(client, unit_id, address)
+            if register_value is not None:
+                print(f"Register {address}: {register_value}")
+            else:
+                print(f"Register {address} not exists")
+
+    # Specify the Modbus address of the device and the range to scan
+    unit_id = 1  # Modbus address of the device
+    start_address = 0  # Start address of the scan
+    end_address = 600  # End address of the scan
+
+    # Scan the registers
+    scan_registers(client, unit_id, start_address, end_address)
+
+    # Close the client connection
+    client.close()
